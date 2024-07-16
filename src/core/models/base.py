44849b8aedf8ec6528832e83c5de87bb6f8a9d63
Kamil Ayupov <ayupovkamil@gmail.com>
2024-07-16 16:11:37 +0300

    Fix

--- src/core/models/base.py
+++ src/core/models/base.py
@@ -10,6 +10,7 @@ from utils import camel_case_to_snake_case
 class Base(DeclarativeBase):
     __abstract__ = True
 
+    # noinspection PyMethodParameters
     @declared_attr.directive
     def __tablename__(cls) -> str:
         return f"{camel_case_to_snake_case(cls.__name__)}s"

471c88ad2749768f976fbead62d9dbcc81e7a359
Kamil Ayupov <ayupovkamil@gmail.com>
2024-07-28 11:15:42 +0300

    Create migration for users table

--- src/core/models/__init__.py
+++ src/core/models/__init__.py
@@ -1,10 +1,9 @@
 __all__ = (
     "db_helper",
     "Base",
-    # "User",
+    "User",
 )
 
 from .db_helper import db_helper
 from .base import Base
-
-# from .user import User
+from .user import User
